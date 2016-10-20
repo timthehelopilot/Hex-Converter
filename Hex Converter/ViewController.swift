a1b39768ff73ca0fd60966a841109d02e38da245
Timothy Barrett <Timmeh@Tims-MacBook.local>
2016-10-20 12:20:46 -0700

    Initial Commit

---
+++ Hex Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hex Converter
+//
+//  Created by Timothy Barrett on 10/20/16.
+//  Copyright © 2016 Timothy Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
